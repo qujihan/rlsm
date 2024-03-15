7575dad3401830bfd6404709a5d53d2225e6f133
qujihan <qujihan@163.com>
2024-03-15 08:24:58 +0800

    block

--- src/block/builder.rs
+++ src/block/builder.rs
@@ -1,41 +1,62 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use bytes::BufMut;
+
 use crate::key::{KeySlice, KeyVec};
 
-use super::Block;
+use super::{Block, SIZEOF_U16};
 
-/// Builds a block.
 pub struct BlockBuilder {
-    /// Offsets of each key-value entries.
     offsets: Vec<u16>,
-    /// All serialized key-value pairs in the block.
     data: Vec<u8>,
-    /// The expected block size.
     block_size: usize,
-    /// The first key in the block
     first_key: KeyVec,
 }
 
 impl BlockBuilder {
-    /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
+    }
+
+    fn estimated_size(&self) -> usize {
+        SIZEOF_U16 + self.offsets.len() * SIZEOF_U16 + self.data.len()
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        assert!(!key.is_empty(), "key must not be empty");
+        if self.estimated_size() + key.len() + value.len() + SIZEOF_U16 * 3 /* key_len, value_len and offset */> self.block_size
+            && !self.is_empty()
+        {
+            return false;
+        }
+
+        self.offsets.push(self.data.len() as u16);
+        self.data.put_u16(key.len() as u16);
+        self.data.put(key.raw_ref());
+        self.data.put_u16(value.len() as u16);
+        self.data.put(value);
+        true
     }
 
-    /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
-    /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        if self.is_empty() {
+            panic!("block should not be empty");
+        }
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
